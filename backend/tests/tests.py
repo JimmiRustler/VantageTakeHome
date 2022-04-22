3845ee88c1bdb76c49dc9a4bf70fc43ab8383b6e
DESKTOP-82JJL7I\Jesse <wmbj7734@gmail.com>
2022-04-21 22:34:36 -0400

    inital test case work

---
+++ backend/tests/tests.py
@@ -0,0 +1,30 @@
+from dataclasses import fields
+from django.test import TestCase
+from adDatabase.models import ad
+
+class EntryModelTest(TestCase):
+    fixtures = ['tests/fixtures/ad.json']
+    def test_url_exists(self):
+        response = self.client.get("/api/ad/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_functions(self):
+        response = self.client.post('/api/ad/',{
+            'id':'',
+            'image':'Man.jpg',
+            'URL':'www.test1.com',
+            'adTitle':'test1',
+            'adInfo':'this is test1',
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_function(self):
+        response = self.client.get("/api/ad/", {'id':'1'})
+        print(response.json)
+        
+        
+
+
+  
+        
+        
